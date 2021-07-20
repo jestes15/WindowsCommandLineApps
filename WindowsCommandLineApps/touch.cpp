25b3904b0701e5363cafe05647d8425afd325fdf
jestes15 <sagepup0620@gmail.com>
2021-07-19 23:16:10 -0500

    Removed an unnecessary function in the touch namespace

--- WindowsCommandLineApps/touch.cpp
+++ WindowsCommandLineApps/touch.cpp
@@ -12,21 +12,6 @@ namespace touch
 		fin.open(path.c_str());	
 		return fin.is_open();
 	}
-
-	auto permissions(fs::perms const perms) -> std::vector<std::string>
-	{
-		std::vector<std::string> permissions;
-		permissions.emplace_back((perms & fs::perms::owner_read) != fs::perms::none ? "r" : "-");
-		permissions.emplace_back((perms & fs::perms::owner_write) != fs::perms::none ? "r" : "-");
-		permissions.emplace_back((perms & fs::perms::owner_exec) != fs::perms::none ? "r" : "-");
-		permissions.emplace_back((perms & fs::perms::group_read) != fs::perms::none ? "r" : "-");
-		permissions.emplace_back((perms & fs::perms::group_write) != fs::perms::none ? "r" : "-");
-		permissions.emplace_back((perms & fs::perms::group_exec) != fs::perms::none ? "r" : "-");
-		permissions.emplace_back((perms & fs::perms::others_read) != fs::perms::none ? "r" : "-");
-		permissions.emplace_back((perms & fs::perms::others_write) != fs::perms::none ? "r" : "-");
-		permissions.emplace_back((perms & fs::perms::others_exec) != fs::perms::none ? "r" : "-");
-		return permissions;
-	}
 }
 
 auto main(int const argc, char const *argv[]) -> int
@@ -37,10 +22,7 @@ auto main(int const argc, char const *argv[]) -> int
 			return -1;
 		}
 		for (auto index = 1; index < argc; index++)
-		{
-			for (auto const &x : touch::permissions(fs::status(".").permissions()))
-				std::cout << x << std::endl;
-			
+		{	
 			if (touch::exist(argv[index]))
 				std::cout << argv[index] << " could not be created. Maybe this file already exists?" << std::endl;
 			else
